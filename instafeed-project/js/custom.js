778f169ec587c95a205cbb339439a3506a1124d6
Barton Thomas <54331697+bartondesign@users.noreply.github.com>
2019-10-02 21:37:33 -0400

    Add files via upload
    instagram feed

---
+++ instafeed-project/js/custom.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+
+    var userFeed = new Instafeed({
+        get: 'user',
+        userId: '2191940001',
+        limit: 12,
+        resolution: 'standard_resolution',
+        accessToken: '2191940001.1677ed0.acd21a1017744a759648a62412ec14a0',
+        sortBy: 'most-recent',
+        template: '<div class="col-lg-3 instaimg"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
+    });
+
+
+    userFeed.run();
+
+    
+    // This will create a single gallery from all elements that have class "gallery-item"
+    $('.gallery').magnificPopup({
+        type: 'image',
+        delegate: 'a',
+        gallery: {
+            enabled: true
+        }
+    });
+
+
+});
